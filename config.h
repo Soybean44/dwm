fc749ce4661fda0bf01f7e6909a825a82c714383
Sovereign Shahid <s.shahid1@share.epsb.ca>
2024-02-27 17:07:58 -0700

    fixed font

--- config.h
+++ config.h
@@ -14,8 +14,8 @@ static const int systraypinningfailfirst =
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;	  /* 0 means no bar */
 static const int topbar = 1;	  /* 0 means bottom bar */
-static const char *fonts[] = {"ComicMono:size=12"};
-static const char dmenufont[] = "ComicMono:size=12";
+static const char *fonts[] = {"ComicShannsMono Nerd Font Mono:size=12"};
+static const char dmenufont[] = "ComicShannsMono Nerd Font Mono:size=12";
 static const char col_gray1[] = "#1e1e2e";
 static const char col_gray2[] = "#313244";
 static const char col_gray3[] = "#bac2de";
