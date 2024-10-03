e385468db8f1ade3265a1c822036b0d1b746d97d
Sovereign Shahid <sovereignshahid@gmail.com>
2024-10-03 10:20:06 -0600

    changed battery code for new laptop

--- scripts/status-laptop.sh
+++ scripts/status-laptop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while :; do
-  BAT=$((($(cat /sys/class/power_supply/BAT1/capacity)+$(cat /sys/class/power_supply/BAT0/capacity))/2))
+  BAT=$(cat /sys/class/power_supply/BAT1/capacity)
   xsetroot -name "$(date +'%I:%M%p') | $BAT%"
   sleep $((60 - $(date +%s) % 60))
 done
