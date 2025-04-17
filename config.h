76e8fd446c29bbcc83f8d8d5f8bc7fc209fdfa70
Sovereign Shahid <sovereignshahid@gmail.com>
2025-04-16 21:46:41 -0600

    added screenshot command

--- config.h
+++ config.h
@@ -123,6 +123,7 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_Escape, spawn, {.v = shutdowncmd}},
     {MODKEY | ControlMask, XK_t, spawn,
      SHCMD("sh /home/soybean44/dotfiles/scripts/rofi-text.sh")},
+    {MODKEY | ShiftMask, XK_s, spawn, SHCMD("flameshot gui")},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
