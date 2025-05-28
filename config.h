3df3324f222d3c79c10e0dabde9d344bacb36994
Sovereign Shahid <sovereignshahid@gmail.com>
2025-05-28 16:03:18 -0600

    fixed dwm config

--- config.h
+++ config.h
@@ -82,7 +82,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"rofi", "-show", "drun"
+static const char *dmenucmd[] = {"rofi", "-show", "drun",
                                  NULL};
 static const char *termcmd[] = {"ghostty", NULL};
 static const char *browsercmd[] = {"zsh", "-c", "$BROWSER", NULL};
